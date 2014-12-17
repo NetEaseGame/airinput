17b36f2362b6a401cabf55cb62854b76649c55d4
codeskyblue <codeskyblue@gmail.com>
2014-12-17 16:53:09 +0800

    add binary

---
+++ dist/run.bat
@@ -0,0 +1,17 @@
+@echo off
+
+echo Android AirInput -- Initializing...
+
+echo Waiting for device to be connected...
+adb.exe wait-for-device
+
+echo - Installing native service...
+adb.exe push air-native "/data/local/tmp/air-native"
+adb.exe shell chmod 755 "/data/local/tmp/air-native"
+
+echo Starting...
+rem adb.exe shell kill -9 "/data/local/tmp/air-native"
+rem adb.exe shell "/data/local/tmp/air-native -daemon"
+adb.exe shell "/data/local/tmp/air-native"
+
+echo Service started successfully.
